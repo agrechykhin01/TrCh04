58a50614492cb032e178845741d03feda5c4c065
Andrey G <agrechykhin01@gmail.com>
2019-07-24 13:56:37 +0300

    test

---
+++ FunWithMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Methods *****\n");
+            // Pass two variables in by value.
+            int x = 9, y = 10;            
+            Add(x, y, out int ans);
+            Console.WriteLine("Before call: X: {0}, Y: {1}", x, y);
+            Console.WriteLine("Answer is: {0}", ans);
+            Console.WriteLine("After call: X: {0}, Y: {1}", x, y);
+        }
+
+        static void Add(int x, int y, out int ans)
+        {
+            ans = x + y;           
+        }
+    }
+}
